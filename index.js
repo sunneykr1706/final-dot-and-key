439178667c50e32d5610b625e7eb9d7d2a358626
sunneykr1706 <sunneykumar309@gmail.com>
2021-11-29 02:15:06 +0530

    first-commit

---
+++ index.js
@@ -0,0 +1,37 @@
+
+/*var visibleDiv = 0;
+
+
+  function showDiv()
+  {
+    $(".grid").hide();
+    $(".grid:eq("+ visibleDiv +")").show();
+   console.log(visibleDiv);
+  }
+  showDiv()
+function showNext()
+{
+  if(visibleDiv== $(".grid").length-1)
+  {
+    visibleDiv = 0;
+    console.log(visibleDiv);
+  }
+  else {
+    visibleDiv++;
+    
+  }
+  showDiv();
+}
+
+
+function showPrev()
+{
+  if (visibleDiv == 0)
+  {
+    visibleDiv= $(".grid").length-1;
+  }
+  else {
+    visibleDiv--;
+  }
+  showDiv();
+}*/
